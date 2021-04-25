1d8371627504ffb6a33e5a9de39ee9c4de2a7e0f
apple <18201497620@163.com>
2021-04-25 17:13:14 +0800

    工厂方法模式完成

---
+++ app/src/main/java/com/example/designpatten/factoryfun/Fruit.java
@@ -0,0 +1,15 @@
+package com.example.designpatten.factoryfun;
+
+/**
+ * create by apple
+ * create on 2021/4/25
+ * description
+ */
+
+/**
+ * 定义水果接口
+ */
+interface Fruit {
+
+    void get();
+}
---
+++ app/src/main/java/com/example/designpatten/factoryfun/PearFruitFactory.java
@@ -0,0 +1,17 @@
+package com.example.designpatten.factoryfun;
+
+/**
+ * create by apple
+ * create on 2021/4/25
+ * description
+ */
+
+/**
+ * 梨工厂，负责创建梨
+ */
+class PearFruitFactory implements FruitFactory {
+    @Override
+    public Fruit getFruit() {
+        return new Pear();
+    }
+}
