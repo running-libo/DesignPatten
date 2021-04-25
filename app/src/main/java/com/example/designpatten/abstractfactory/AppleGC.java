3d404025ad99b62c1ae5e4c70a1a9abdc6462a34
apple <18201497620@163.com>
2021-04-25 23:04:44 +0800

    抽象工厂模式完成

---
+++ app/src/main/java/com/example/designpatten/abstractfactory/AppleGC.java
@@ -0,0 +1,13 @@
+package com.example.designpatten.abstractfactory;
+
+/**
+ * create by apple
+ * create on 2021/4/25
+ * description
+ */
+class AppleGC extends Apple {
+    @Override
+    public void get() {
+        System.out.println("生产国产苹果");
+    }
+}
---
+++ app/src/main/java/com/example/designpatten/abstractfactory/Fruit.java
@@ -0,0 +1,15 @@
+package com.example.designpatten.abstractfactory;
+
+/**
+ * create by apple
+ * create on 2021/4/25
+ * description
+ */
+
+/**
+ * 定义水果接口
+ */
+interface Fruit {
+
+    void get();
+}
