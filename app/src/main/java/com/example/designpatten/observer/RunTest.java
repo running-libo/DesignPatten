c4dc4c4131228b7358cbc0ffe0d0eafa1bc90ef9
apple <18201497620@163.com>
2021-04-27 01:03:23 +0800

    观察者模式完成

---
+++ app/src/main/java/com/example/designpatten/observer/RunTest.java
@@ -0,0 +1,21 @@
+package com.example.designpatten.observer;
+
+/**
+ * create by apple
+ * create on 2021/4/27
+ * description
+ */
+class RunTest {
+
+    public static void main(String[] args) {
+
+        //创建一个观察者文章作者
+        ArticelOperation article = new ArticelOperation();
+        //添加各个观察者进行监听
+        article.addObserver(new Observer());
+        article.addObserver(new Observer2());
+
+        //作者发布文章
+        article.publish("Android进阶解密", "解读Android源码", "刘皇叔");
+    }
+}
